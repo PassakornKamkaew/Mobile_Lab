60b105a93a79d024ecfd640cc552515b795f605a
Peak1z <88501407+PassakornKamkaew@users.noreply.github.com>
2023-10-12 13:55:40 +0700

    Add files via upload

---
+++ mobilelab10/database/firebaseDB.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/compat";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAr6Stju_3vPQduU7as77-bO8OX4YqfKxw",
+  authDomain: "lab10-1cd71.firebaseapp.com",
+  projectId: "lab10-1cd71",
+  storageBucket: "lab10-1cd71.appspot.com",
+  messagingSenderId: "763136274782",
+  appId: "1:763136274782:web:277aff7f43b1276ef89fcf",
+  measurementId: "G-7719F4DK5L"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+export default firebase;
